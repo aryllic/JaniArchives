408a647de3ef896e931c968acdfd7ad8b5e8e30b
aryllic <59052885+aryllic@users.noreply.github.com>
2021-09-27 20:06:02 +0200

    Add files via upload

--- js/editbuttons.js
+++ js/editbuttons.js
@@ -1,15 +1,61 @@
-var maincontent, editbuttons, buttonarray
+var body, maincontent;
 
 function starteditbuttons() {
-    maincontent = document.getElementById("main-content")
+    body = document.getElementsByTagName("body")[0];
+    maincontent = document.getElementById("main-content");
+    items = document.getElementsByClassName("item");
+    itemarray = [].slice.call(items);
     editbuttons = document.getElementsByClassName("edit-button");
-    buttonarray = [].slice.call(editbuttons);
+    editbuttonarray = [].slice.call(editbuttons);
+    addbuttons = document.getElementsByClassName("add-button");
+    addbuttonarray = [].slice.call(addbuttons);
+    savebuttons = document.getElementsByClassName("save-button");
+    savebuttonarray = [].slice.call(savebuttons);
+    itemforms = document.getElementsByClassName("item-form");
+    itemformarray = [].slice.call(itemforms);
 
-    buttonarray.forEach(btn => {
-        btn.hidden = true
+    itemarray.forEach(item => {
+        item.addEventListener("mousedown", function() {
+            item.holding = true;
+            console.log(item.holding);
+        });
+
+        item.addEventListener("mouseup", function() {
+            item.holding = false;
+            console.log(item.holding);
+        });
+    });
+
+    editbuttonarray.forEach(btn => {
+        btn.hidden = false;
         
         btn.addEventListener("click", function() {
-            console.log("Edit")
+            console.log(body.innerHTML);
+        });
+    });
+
+    addbuttonarray.forEach(btn => {
+        btn.hidden = false;
+        
+        btn.addEventListener("click", function() {
+            maincontent.innerHTML += "<div class='item'>\n<form class='item-form'>\n<input type='text' placeholder='Item' id='itemname'>\n</form>\n</div>\n";
+            starteditbuttons();
+        });
+    });
+
+    savebuttonarray.forEach(btn => {
+        btn.hidden = true;
+        
+        btn.addEventListener("click", function() {
+            console.log("Save");
+        });
+    });
+
+    itemformarray.forEach(frmitem => {
+        
+        frmitem.addEventListener("submit", function(frm) {
+            frm.preventDefault();
+            frmitem.parentElement.innerHTML = "\n<a href=''>" + frmitem.itemname.value + "</a>";
         });
     });
 };
