68255ed4dd873140d32796dc8053524d9f83251f
aryllic <59052885+aryllic@users.noreply.github.com>
2021-11-26 20:34:38 +0100

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,19 @@
-const fetchurl = "https://discord.com/api/webhooks/862332234416259092/g7KtpVu_A3ofgMMhxJ78mwV3sIjtns4MXnybCySaAMVprnvIPEYmQUZ4fN3Rx-5GaTXn"
-var msgform, chatbox;
+const fetchurl = "https://discord.com/api/webhooks/913863782437101569/l_JLCLy8QWQMTecRyf_fKOv9k785e8JiHa2g9A_UlhKT6CDj3SPhgIOXZfbam6gevi6N"
+var msgform, chatbox, ip;
+
+function SetIP(IP) {
+    if (IP != "85.90.159.118") {
+        ip = IP;
+        chatbox.innerHTML += "<div class='msg-ann'>" + IP + " entered the chat!" + "</div>";
+    } else {
+        ip = "**OWNER**";
+        chatbox.innerHTML += "<div class='msg-ann'>" + "The owner entered the chat!" + "</div>";
+    }
+}
+
+fetch('https://api.ipify.org/?format=json')
+    .then(results => results.json())
+    .then(data => SetIP(data.ip))
 
 function sortmsgs() {
     for (var i = chatbox.children.length - 1; i >= 0; i--) {
@@ -26,6 +40,14 @@ function startchat() {
 
         if (msgform.messagebox.value != "") {
             chatbox.innerHTML += "<div class='msg-cl'>" + msgform.messagebox.value + "</div>";
+
+            const msg = { "content": "New message from " + ip + ": " + msgform.messagebox.value }
+            fetch(fetchurl + "?wait=true",
+                {
+                    "method": "POST",
+                    "headers": { "content-type": "application/json" },
+                    "body": JSON.stringify(msg)
+                });
         };
 
         msgform.messagebox.value = "";
